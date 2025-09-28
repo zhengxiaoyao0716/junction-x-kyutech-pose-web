5c0c02c112e96025b6b00cc247165cb281eed0cb
zhengxiaoyao0716 <zhengxiaoyao0716@outlook.com>
2025-09-28 11:11:20 +0900

    add bg animation

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,7 @@
 	import '$lib/assets/styles/root.css';
 	import '$lib/assets/styles/theme.css';
 	import { iconFontCssUrl } from '$lib/icon.svelte';
+	import { type ClassValue } from 'svelte/elements';
 
 	let { data, children } = $props();
 
@@ -16,16 +17,29 @@
 
 <div id="root">
 	<div class="bg full" style:background-image="url('/images/bg0.webp')"></div>
-	{@render background('0', '0')}
-	{@render background('60%', '50%')}
+	{@render background({ cls: 'wander1' })}
+	{@render background({ cls: 'wander2' })}
+	<div
+		id="nasa"
+		{@attach () => {
+			// @ts-ignore-line
+			globalThis.particlesJS.load('nasa', '/styles/particlesjs-config.json');
+		}}
+	></div>
 	{@render children?.()}
 </div>
 
-{#snippet background(left: string, top: string, width = '512px', height = '512px')}
+{#snippet background({
+	width = '512px',
+	height = '512px',
+	cls = []
+}: {
+	width?: string;
+	height?: string;
+	cls?: ClassValue;
+})}
 	<div
-		class="bg"
-		style:left
-		style:top
+		class={['bg', cls]}
 		style:width
 		style:height
 		style:background-image="url('/images/bg1.webp')"
@@ -48,6 +62,40 @@
 		top: 0;
 		background-repeat: repeat;
 		background-size: auto;
+	}
+	.bg:global(.wander1) {
+		left: 0;
+		top: 0;
+		animation: wander1 17s infinite linear;
+	}
+	.bg:global(.wander2) {
+		right: 0%;
+		bottom: 0%;
+		animation: wander2 13s infinite linear;
+	}
+	/* prettier-ignore */
+	@keyframes wander1 {
+		0%   { translate: 0 0; }
+		30%  { translate: 80svw 20svw; }
+		70%  { translate: 20svw 80svw; }
+		100% { translate: 0 0; }
+	}
+	/* prettier-ignore */
+	@keyframes wander2 {
+		0%   { translate: 0 0; }
+		20%  { translate: -10svw -30vw; }
+		45%  { translate: -60svw -10vw; }
+		75%  { translate: -40svw 10vw; }
+		100% { translate: 0 0; }
+	}
+	#nasa {
+		position: absolute;
+		pointer-events: none;
+		width: 100%;
+		height: 100%;
+		left: 0;
+		top: 0;
+		opacity: 0.3;
 	}
 
 	:global(body) {
